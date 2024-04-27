217e9adbfb8d67cafdc860f0098ac5b4fd8c57af
codewithsaidul <codewithsaidul@gmail.com>
2024-04-27 13:36:47 +0600

    React TypeWritter added on craft items section title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useLoaderData } from "react-router-dom"
 import Banner from "../components/Banner"
 import CraftCard from "../components/CraftCard";
+import { Typewriter } from "react-simple-typewriter";
 
 
 const Home = () => {
@@ -11,10 +12,24 @@ const Home = () => {
         <Banner />
 
         <div>
-          <h2 className="text-center font-title text-6xl text-primary font-bold my-16">Latest Jute Craft Items</h2>
+          <h2 className="text-center font-title text-6xl text-black font-bold my-16">
+            Latest{" "}
+            <span style={{ color: "#4ad66d", fontWeight: "bold" }}>
+              {/* Style will be inherited from the parent element */}
+              <Typewriter
+                words={["Jute Craft Items", "Wooden Craft Items"]}
+                loop={5}
+                cursor
+                cursorStyle=""
+                typeSpeed={70}
+                deleteSpeed={50}
+                delaySpeed={1000}
+
+              />
+            </span>
+          </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-            {craftItems.slice(0, 6
-            ).map((craftItem) => (
+            {craftItems.slice(0, 6).map((craftItem) => (
               <CraftCard key={craftItem.id} craftItem={craftItem}></CraftCard>
             ))}
           </div>
