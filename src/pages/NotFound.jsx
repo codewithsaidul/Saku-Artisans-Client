fb7451654db1c11f49dbf5826580c2906adaab28
codewithsaidul <codewithsaidul@gmail.com>
2024-04-30 22:11:15 +0600

    404 not found page background problem solev

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,17 +1,19 @@
 import { Link } from "react-router-dom";
-import BG from "../assets/Moon.svg";
+
 
 const NotFound = () => {
   return (
     <div>
       <div
         className="w-full min-h-screen flex justify-center items-center px-5"
-        style={{ backgroundImage: `url(${BG})` }}
+        style={{ backgroundImage: "url(/Moon.svg)" }}
       >
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
             <h1 className="mb-5 text-5xl font-bold">404</h1>
-            <h3 className="mb-5 text-5xl text-primary font-title font-bold">Page Not Found.</h3>
+            <h3 className="mb-5 text-5xl text-primary font-title font-bold">
+              Page Not Found.
+            </h3>
             <p className="mb-5 text-gray-400 font-medium">
               Sorry, the page you are looking for doesn’t exist. Go out, take a
               run around the block, or tap the button below.
